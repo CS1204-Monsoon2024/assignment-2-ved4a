db711f674112db438e93ff84f349a70065acdb74
Vedaa Anand <work.vedaa@gmail.com>
2024-10-18 17:52:33 +0530

    feat: Create struct for entries in HT

--- HashTable.cpp
+++ HashTable.cpp
@@ -3,3 +3,17 @@
 #include <cmath>
 
 using namespace std;
+
+class HashTable
+{
+private:
+    struct Entry
+    {
+        int key;
+        int value;
+        bool is_deleted;
+
+        Entry(){key = 0, value = 0, is_deleted = false;} // default constructor
+        Entry(int key, int value) {is_deleted = false;} // constructor w initializing
+    }
+}
