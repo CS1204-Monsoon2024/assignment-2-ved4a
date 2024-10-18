fd9b56cbc4809466d846ec1791c4fc64fbf9037c
Vedaa Anand <work.vedaa@gmail.com>
2024-10-18 17:56:23 +0530

    feat: Add private members including hash table, size, capacity, LFT

--- HashTable.cpp
+++ HashTable.cpp
@@ -13,7 +13,14 @@ private:
         int value;
         bool is_deleted;
 
-        Entry(){key = 0, value = 0, is_deleted = false;} // default constructor
+        Entry() {key = 0, value = 0, is_deleted = false;} // default constructor
         Entry(int key, int value) {is_deleted = false;} // constructor w initializing
-    }
-}
+    };
+
+    vector<Entry> table; // HT as a vector of Entry objects
+    int curr_size;
+    int capacity;
+    double alpha = 0.8; // load factor
+public:
+
+};
