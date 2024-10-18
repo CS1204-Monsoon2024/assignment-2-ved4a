bce17a4cd63264f2b61078322d72a0d77bdde938
Vedaa Anand <work.vedaa@gmail.com>
2024-10-18 18:00:59 +0530

    feat: Implement hash fun and check if num is prime

--- HashTable.cpp
+++ HashTable.cpp
@@ -21,6 +21,23 @@ private:
     int curr_size;
     int capacity;
     double alpha = 0.8; // load factor
+
+    int hash(int key)
+    {
+        return key % capacity;
+    };
+
+    bool is_prime(int num)
+    {
+        if (num <= 1) return false;
+        if (num == 2) return true;
+        if (num % 2 == 0) return false;
+        for (int i = 3; i * i <= num; i += 2) { // until sqrt num
+            if (num % i == 0) return false;
+        }
+        return true;
+    };
+
 public:
 
 };
